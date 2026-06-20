a5dedf119f2133d3547a93c87f3a01fe85e7b2ec
qweaad1 <127671966+qweaad1@users.noreply.github.com>
2026-06-20 13:54:43 +0700

    Update pet-ineraction.lua

--- sample/pet-ineraction.lua
+++ sample/pet-ineraction.lua
@@ -6,26 +6,13 @@ local Player = Players.LocalPlayer
 local Character = Player.Character or Player.CharacterAdded:Wait()
 local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
 
-local Showers = function(Name)
-    
-    Player.Character.HumanoidRootPart.CFrame = Workspace.HouseInteriors.blueprint:FindFirstChildWhichIsA('Model'):GetModelCFrame():ToWorldSpace(CFrame.new(0, 0, -6))
-    
-    for i, v in pairs(Workspace.HouseInteriors.furniture:GetChildren()) do
-   
-        if v:FindFirstChildWhichIsA("Model") and v:FindFirstChildWhichIsA("Model").Name == "CheapPetBathtub" then
-          print(v:FindFirstChildWhichIsA("Model").Name)
-            local String = string.split(v.Name, "true/")[2]
-            print(String)
-          return String
-        end
-    end
-    
-end
+local Dirty = function(Name)  Player.Character.HumanoidRootPart.CFrame = Workspace.HouseInteriors.blueprint:FindFirstChildWhichIsA('Model'):GetModelCFrame():ToWorldSpace(CFrame.new(0, 0, -6)) for i, v in pairs(Workspace.HouseInteriors.furniture:GetChildren()) do  if v:FindFirstChildWhichIsA("Model") and v:FindFirstChildWhichIsA("Model").Name == "CheapPetBathtub" then   local String = string.split(v.Name, "true/")[2]   return String end end end
+local Sleepy = function(Name)  Player.Character.HumanoidRootPart.CFrame = Workspace.HouseInteriors.blueprint:FindFirstChildWhichIsA('Model'):GetModelCFrame():ToWorldSpace(CFrame.new(0, 0, -6)) for i, v in pairs(Workspace.HouseInteriors.furniture:GetChildren()) do  if v:FindFirstChildWhichIsA("Model") and v:FindFirstChildWhichIsA("Model").Name == "BasicCrib" then   local String = string.split(v.Name, "true/")[2]   return String end end end
 
 
      game:GetService("ReplicatedStorage").API["HousingAPI/ActivateFurniture"]:InvokeServer(table.unpack({
     [1] = game:GetService("Players").LocalPlayer,
-    [2] = "" .. Showers(),
+    [2] = "" .. Dirty(),
     [3] = "UseBlock",
     [4] = {
         ["cframe"] = HumanoidRootPart.CFrame,
