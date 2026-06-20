102d0601ad85e8777f0cbda008b9df4361ef6261
qweaad1 <127671966+qweaad1@users.noreply.github.com>
2026-06-20 15:35:07 +0700

    Update pet-ineraction.lua

--- sample/pet-ineraction.lua
+++ sample/pet-ineraction.lua
@@ -10,12 +10,9 @@ local Dirty = function(Name)  Player.Character.HumanoidRootPart.CFrame = Workspa
 local Sleepy = function(Name)  Player.Character.HumanoidRootPart.CFrame = Workspace.HouseInteriors.blueprint:FindFirstChildWhichIsA('Model'):GetModelCFrame():ToWorldSpace(CFrame.new(0, 0, -6)) for i, v in pairs(Workspace.HouseInteriors.furniture:GetChildren()) do  if v:FindFirstChildWhichIsA("Model") and v:FindFirstChildWhichIsA("Model").Name == "BasicCrib" then   local String = string.split(v.Name, "true/")[2]   return String end end end
 
 
-     game:GetService("ReplicatedStorage").API["HousingAPI/ActivateFurniture"]:InvokeServer(table.unpack({
-    [1] = game:GetService("Players").LocalPlayer,
-    [2] = "" .. Dirty(),
-    [3] = "UseBlock",
-    [4] = {
-        ["cframe"] = HumanoidRootPart.CFrame,
-    },
-    [5] = workspace:FindFirstChild("Pets"):GetChildren()[1],
-}))
+    
+    function ineraction(FurnitureName, TypeInteraction)
+     game:GetService("ReplicatedStorage").API["HousingAPI/ActivateFurniture"]:InvokeServer(table.unpack({  [1] = game:GetService("Players").LocalPlayer,  [2] = "" .. FurnitureName,  [3] = ""..TypeInteraction,  [4] = { ["cframe"] = HumanoidRootPart.CFrame,   },  [5] = workspace:FindFirstChild("Pets"):GetChildren()[1],}))
+
+end
+ineraction(Dirty(),"UseBlock")
