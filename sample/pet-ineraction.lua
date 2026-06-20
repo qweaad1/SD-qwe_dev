6631d14c0847703fc11f05af33070064aceb31b6
qweaad1 <127671966+qweaad1@users.noreply.github.com>
2026-06-20 15:37:24 +0700

    Update pet-ineraction.lua

--- sample/pet-ineraction.lua
+++ sample/pet-ineraction.lua
@@ -8,6 +8,8 @@ local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
 
 local Dirty = function(Name)  Player.Character.HumanoidRootPart.CFrame = Workspace.HouseInteriors.blueprint:FindFirstChildWhichIsA('Model'):GetModelCFrame():ToWorldSpace(CFrame.new(0, 0, -6)) for i, v in pairs(Workspace.HouseInteriors.furniture:GetChildren()) do  if v:FindFirstChildWhichIsA("Model") and v:FindFirstChildWhichIsA("Model").Name == "CheapPetBathtub" then   local String = string.split(v.Name, "true/")[2]   return String end end end
 local Sleepy = function(Name)  Player.Character.HumanoidRootPart.CFrame = Workspace.HouseInteriors.blueprint:FindFirstChildWhichIsA('Model'):GetModelCFrame():ToWorldSpace(CFrame.new(0, 0, -6)) for i, v in pairs(Workspace.HouseInteriors.furniture:GetChildren()) do  if v:FindFirstChildWhichIsA("Model") and v:FindFirstChildWhichIsA("Model").Name == "BasicCrib" then   local String = string.split(v.Name, "true/")[2]   return String end end end
+local WaterH = function(Name)  Player.Character.HumanoidRootPart.CFrame = Workspace.HouseInteriors.blueprint:FindFirstChildWhichIsA('Model'):GetModelCFrame():ToWorldSpace(CFrame.new(0, 0, -6)) for i, v in pairs(Workspace.HouseInteriors.furniture:GetChildren()) do  if v:FindFirstChildWhichIsA("Model") and v:FindFirstChildWhichIsA("Model").Name == "PetWaterBowl" then   local String = string.split(v.Name, "true/")[2]   return String end end end
+local FoodH = function(Name)  Player.Character.HumanoidRootPart.CFrame = Workspace.HouseInteriors.blueprint:FindFirstChildWhichIsA('Model'):GetModelCFrame():ToWorldSpace(CFrame.new(0, 0, -6)) for i, v in pairs(Workspace.HouseInteriors.furniture:GetChildren()) do  if v:FindFirstChildWhichIsA("Model") and v:FindFirstChildWhichIsA("Model").Name == "PetFoodBowl" then   local String = string.split(v.Name, "true/")[2]   return String end end end
 
 
     
@@ -15,4 +17,4 @@ local Sleepy = function(Name)  Player.Character.HumanoidRootPart.CFrame = Worksp
      game:GetService("ReplicatedStorage").API["HousingAPI/ActivateFurniture"]:InvokeServer(table.unpack({  [1] = game:GetService("Players").LocalPlayer,  [2] = "" .. FurnitureName,  [3] = ""..TypeInteraction,  [4] = { ["cframe"] = HumanoidRootPart.CFrame,   },  [5] = workspace:FindFirstChild("Pets"):GetChildren()[1],}))
 
 end
-ineraction(Dirty(),"UseBlock")
+ineraction(FoodH(),"UseBlock")
