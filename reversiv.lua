d4365c8c496c1e09d727149f7b375ad6ffda5f61
qweaad1 <127671966+qweaad1@users.noreply.github.com>
2026-07-07 19:27:39 +0700

    Update reversiv.lua

--- reversiv.lua
+++ reversiv.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Klinac/scripts/main/utopia_spy.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/peyton2465/Dex/master/out.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/DinosaurXxX/b757fe011e7e600c0873f967fe427dc2/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4"))()
