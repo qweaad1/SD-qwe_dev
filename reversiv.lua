8f6fcaf9d31b75eaaa3be49279e0044a6f8667c6
qweaad1 <127671966+qweaad1@users.noreply.github.com>
2026-07-07 16:40:50 +0700

    Create reversiv.lua

---
+++ reversiv.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Klinac/scripts/main/utopia_spy.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/peyton2465/Dex/master/out.lua"))()
